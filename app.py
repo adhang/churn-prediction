728d3267c85d3644275713034541d5507c5aa9bd
adhang <29569758+adhang@users.noreply.github.com>
2022-04-15 21:20:52 +0700

    add function to plot SHAP (explainable AI)

--- app.py
+++ app.py
@@ -7,8 +7,71 @@ Created on Thu Apr 14 15:05:36 2022
 from flask import Flask, redirect, url_for, request, render_template
 import pandas as pd
 import joblib
+import matplotlib.pyplot as plt
+import shap
+from imblearn.pipeline import Pipeline
+from sklearn.impute import SimpleImputer
+from sklearn.compose import ColumnTransformer
 
 
+#load the trained model.
+path = 'trained_model.pkl'
+model = joblib.load(path)
+
+column_list = ['tenure', 'monthly_charges', 'total_charges', 'dependents_yes',
+           'internet_service_fiber_optic', 'internet_service_no',
+           'online_security_yes', 'tech_support_yes',
+           'contract_month_to_month', 'contract_one_year',
+           'contract_two_year', 'paperless_billing_yes',
+           'payment_method_electronic_check']
+
+transformer = Pipeline(steps=[('preprocessor', model.named_steps['preprocessor']),
+                              ('feature_selection', model.named_steps['feature_selection'])])
+    
+def shap_plot(input_df):
+    # transform the input dataframe
+    input_transformed = transformer.transform(input_df)
+    input_transformed = pd.DataFrame(input_transformed, columns=column_list)
+    
+    # inverse transform the numeric input
+    input_numeric = input_transformed.iloc[:, :3]
+    inverse_numeric = transformer.named_steps['preprocessor'].transformers_[0][1].named_steps['scaler'].inverse_transform(input_numeric)
+    
+    # shap explainer
+    explainer = shap.Explainer(model.named_steps['estimator'])
+    shap_values = explainer(input_transformed)
+    expected_value = explainer.expected_value[1]
+    
+    # change shap values to 1-d array
+    shap_values.values = shap_values.values[:,:,1].flatten()
+    shap_values.base_values = shap_values.base_values[0,1]
+    
+    # overwrite the scaled numerical data with the original data
+    shap_values.data[0][0:3] = inverse_numeric.flatten()
+    shap_values.data = shap_values.data.flatten()
+    
+    # calculate final shap values
+    shap_values_total = shap_values.values.sum() + shap_values.base_values
+    
+    shap_status = ''
+    if (shap_values_total > shap_values.base_values):
+        print(shap_values_total, 'Churn')
+        shap_status = 'greater than'
+    elif (shap_values_total < shap_values.base_values):
+        print(shap_values_total, 'Retain')
+        shap_status = 'less than'
+    else:
+        print('Cannot say')
+        shap_status = 'the same as'
+        
+    # waterfall plot
+    shap.waterfall_plot(shap_values, max_display=20, show=False)
+    # plt.tight_layout()
+    plt.savefig('static/images/waterfall.png', bbox_inches='tight')
+    plt.show()
+    
+    return expected_value, shap_values_total, shap_status
+    
 app = Flask(__name__)
 
 @app.route("/")
@@ -64,26 +127,26 @@ def prediction_result():
             'streaming_movies':streaming_movies,
             }
     
-    df_input = pd.DataFrame(data, index=[0])
+    input_df = pd.DataFrame(data, index=[0])
+    expected_value, shap_values_total, shap_status = shap_plot(input_df)
     
-    #load the trained model.
-    path = 'trained_model.pkl'
-    loaded_model= joblib.load(path)
+    y_pred = model.predict(input_df)
     
-    result = loaded_model.predict(df_input)
-    
-    for i in result:
-      int_result = int(i)
-      if (int_result == 0):
-        decision = 'Retain'
-      elif (int_result==1):
-        decision = 'Churn'
+    for i in y_pred:
+      y_pred_int = int(i)
+      if (y_pred_int == 0):
+        prediction_result = 'Retain'
+      elif (y_pred_int == 1):
+        prediction_result = 'Churn'
       else:
-        decision = 'Not defined'
+        prediction_result = 'Not defined'
         
     #return the output and load result.html
-    return render_template('result.html', status=decision)
-    #return render_template('index.html', status=decision)
+    return render_template('result.html',
+                           prediction_result=prediction_result,
+                           expected_value=expected_value.round(3),
+                           shap_values_total=shap_values_total.round(3),
+                           shap_status=shap_status)
 
 if __name__ == "__main__":
     app.run()
